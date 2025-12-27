1ddd9059165fa57fe0b325c61ec3a680e8fee76b
Yingfan Geng <yingfan.geng22@imperial.ac.uk>
2025-12-27 20:26:21 +0000

    notebook clean up

--- solver_code.py
+++ solver_code.py
@@ -29,42 +29,40 @@ class convection_diffusion_equation:
 
 
 
-    def solver_tdma(self, number_of_grid, scheme):
-
-        self.delta_x = self.L/(number_of_grid-1)        # end points are half-cell
-        self.Pe_local = self.rho * self.u * self.delta_x / self.gamma
-        
+    def solver_tdma(self):
         
         # initialise the P, Q and phi lists
-        Q_list = np.zeros(number_of_grid)
-        P_list = np.zeros(number_of_grid)
-        phi_list = np.zeros(number_of_grid)
+        Q_list = np.zeros(self.number_of_grid)
+        P_list = np.zeros(self.number_of_grid)
+        phi_list = np.zeros(self.number_of_grid)
         
         # apply the boundary conditions
         Q_list[0] = self.phi_0
         P_list[0] = 0
-        Q_list[number_of_grid-1] = self.phi_L
-        P_list[number_of_grid-1] = 0
+        Q_list[self.number_of_grid-1] = self.phi_L
+        P_list[self.number_of_grid-1] = 0
         phi_list[0] = self.phi_0
-        phi_list[number_of_grid-1] = self.phi_L
+        phi_list[self.number_of_grid-1] = self.phi_L
 
         # forward sweep to calculate P and Q at each node
-        # please note that the calculation a, b and c is implemented inside the loop as a more general code implementation
-        # in this specific flow problem, since all fluid properties and flow velocities are constant across the domain
-        # these coefficients actually constants and need to calculate once only outside the TDMA loop
+        # please note that the calculation of a, b and c is implemented 
+        # inside the loop as a more general code implementation. in this 
+        # specific flow problem, since all fluid properties and flow velocities 
+        # are constant across the domain, these coefficients actually constants 
+        # and need to calculate once only outside the TDMA loop
 
-        for i in range(1, number_of_grid-1):
-            if scheme == "CDS":
+        for i in range(1, self.number_of_grid-1):
+            if self.scheme == "CDS":
                 b = self.gamma/self.delta_x - self.rho*self.u/2
                 c = self.gamma/self.delta_x + self.rho*self.u/2
                 a = b + c
 
-            elif scheme == "UDS":
+            elif self.scheme == "UDS":
                 b = self.gamma/self.delta_x + max(-(self.rho*self.u), 0)
                 c = self.gamma/self.delta_x + max(self.rho*self.u, 0)
                 a = b + c
 
-            elif scheme == "PLDS":
+            elif self.scheme == "PLDS":
             
                 b = self.gamma/self.delta_x * max(0, (1 - 0.1 * abs(self.Pe_local)) ** 5) + max(-(self.rho*self.u), 0)
                 c = self.gamma/self.delta_x * max(0, (1 - 0.1 * abs(self.Pe_local)) ** 5) + max(self.rho*self.u, 0)
@@ -77,7 +75,7 @@ class convection_diffusion_equation:
             Q_list[i] = c*Q_list[i-1] / (a - c*P_list[i-1])
 
         # backward sweep to calculate phi at each node
-        for i in reversed(range(1, number_of_grid-1)):
+        for i in reversed(range(1, self.number_of_grid-1)):
             phi_list[i] = P_list[i]*phi_list[i+1] + Q_list[i]
 
         return phi_list
@@ -97,6 +95,14 @@ class convection_diffusion_equation:
         error = 100 * np.mean(np.abs((phi_actual - phi_analytic) / phi_analytic))
         
         return error
+
+
+    def calculation_of_local_peclet_number(self):
+        
+        Pe_local = self.rho * self.u * self.delta_x / self.gamma
+
+        return Pe_local
+
 
 
 
@@ -148,11 +154,9 @@ class convection_diffusion_equation:
         self.u = u
         self.number_of_grid = number_of_grid
         x = np.linspace(0, self.L, self.number_of_grid)
-        delta_x = x[1] - x[0]
-        print(delta_x)
+        self.delta_x = self.L/(self.number_of_grid-1)       # end points are half-cell
         phi_analytic = self.analytical_phi(x)
 
-        
         # plot the analytical solution smoothly
         x_for_analytic_plot = np.linspace(0, self.L, 999)
         phi_analytic_plot = self.analytical_phi(x_for_analytic_plot)
@@ -162,11 +166,12 @@ class convection_diffusion_equation:
         ax.plot(x_for_analytic_plot, phi_analytic_plot, label = 'analytic', ls = '--', zorder = 10)
 
         for scheme in scheme_list: 
-            Pe_local = self.rho * self.u * delta_x / self.gamma
+            self.scheme = scheme
+            self.Pe_local = self.calculation_of_local_peclet_number()
             start_time = time.time()
-            phi_actual = self.solver_tdma(number_of_grid, scheme)
+            phi_actual = self.solver_tdma()
             end_time = time.time()
-            print(f"Execution time: {end_time - start_time} seconds for {scheme}.")
+            print(f"Computation time: {end_time - start_time} seconds for {scheme}.")
             error = self.error_calculation(phi_actual, phi_analytic)
             ax.plot(x, phi_actual, marker = 'o', label = f'{scheme} (error = {error:.2f}%)')
         ax.set_xlim(0,1)
@@ -177,7 +182,7 @@ class convection_diffusion_equation:
         ax.set_ylabel('$\phi$', fontsize = 16)
         ax.tick_params(axis='both', which='major', labelsize=16)
 
-        self.save_fig_custom(fig, file_path='figures', file_name=f'phi_plot_grid_{number_of_grid}_u_{u}_pe_{Pe_local:.2f}', overwrite=True, dpi = 500)
+        self.save_fig_custom(fig, file_path='figures', file_name=f'phi_plot_grid_{self.number_of_grid}_u_{u}_pe_{self.Pe_local:.2f}', overwrite=True, dpi = 500)
 
 
 
@@ -187,25 +192,29 @@ class convection_diffusion_equation:
         ax.set_prop_cycle(color=plt.cm.Set1.colors[1:])
         
         for scheme in scheme_list: 
+            self.scheme = scheme
             error_list = []
             delta_x_list = []
             
             if mode == 'grid_size':
                 self.u = u_list[0]
                 for number_of_grid in number_of_grid_list:
-                    x = np.linspace(0, self.L, number_of_grid)
-                    delta_x = x[1] - x[0]
-                    delta_x_list.append(delta_x)
+                    self.number_of_grid = number_of_grid
+                    x = np.linspace(0, self.L, self.number_of_grid)
+                    self.delta_x = self.L/(self.number_of_grid-1)
+                    delta_x_list.append(self.delta_x)
                     phi_analytic = self.analytical_phi(x)
-                    phi_actual = self.solver_tdma(number_of_grid, scheme)
+                    self.Pe_local = self.calculation_of_local_peclet_number()
+                    phi_actual = self.solver_tdma()
                     error = self.error_calculation(phi_actual, phi_analytic)
                     error_list.append(error)
-                ax.plot(delta_x_list, error_list, label = f'{scheme}')
+                ax.plot(delta_x_list, error_list, label = f'{self.scheme}')
                 ax.set_xscale('log')
+                ax.set_yscale('log')
                 ax.set_xlabel('Grid Size $\delta x$ (m)', fontsize = 16)
 
-                def dx_to_pe(dx):
-                    return self.rho * self.u * dx / self.gamma
+                def dx_to_pe(delta_x):
+                    return self.rho * self.u * delta_x / self.gamma
 
                 def pe_to_dx(pe):
                     return pe * self.gamma / (self.rho * self.u)
@@ -215,30 +224,34 @@ class convection_diffusion_equation:
                 secax.tick_params(axis='x', labelsize=16)
 
             elif mode == 'flow_velocity':
-                number_of_grid = number_of_grid_list[0]
+                self.number_of_grid = number_of_grid_list[0]
                 for u in u_list:
                     self.u = u
-                    x = np.linspace(0, self.L, number_of_grid)
-                    delta_x = x[1] - x[0]
+                    x = np.linspace(0, self.L, self.number_of_grid)
+                    self.delta_x = self.L/(self.number_of_grid-1)
                     phi_analytic = self.analytical_phi(x)
-                    phi_actual = self.solver_tdma(number_of_grid, scheme)
+                    self.Pe_local = self.calculation_of_local_peclet_number()
+                    phi_actual = self.solver_tdma()
                     error = self.error_calculation(phi_actual, phi_analytic)
                     error_list.append(error)
-                ax.plot(u_list, error_list, label = f'{scheme}')
+                ax.plot(u_list, error_list, label = f'{self.scheme}')
                 ax.set_xlabel('Flow Velocity $u$ (m/s)', fontsize = 16)
                 
                 def u_to_pe(u):
-                    return self.rho * u * delta_x / self.gamma
+                    return self.rho * u * self.delta_x / self.gamma
 
                 def pe_to_u(pe):
-                    return pe * self.gamma / (self.rho * delta_x)
+                    return pe * self.gamma / (self.rho * self.delta_x)
                 
                 secax = ax.secondary_xaxis('top', functions=(u_to_pe, pe_to_u))
                 secax.set_xlabel('Local Péclet Number $Pe$', fontsize=16)
                 secax.tick_params(axis='x', labelsize=16)
 
+            else:
+                raise NotImplementedError
+            
         ax.grid(ls = ':')
-        ax.legend(loc = 'upper left', fontsize = 16)
+        ax.legend(loc = 'best', fontsize = 16)
         
         ax.set_ylabel('Percentage Mean Error', fontsize = 16)
         ax.tick_params(axis='both', which='major', labelsize=16)
